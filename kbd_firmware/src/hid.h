dc1dff32671d92e086c3e1b005f625bc3e761b54
Paul Klinger <klinger.paul@gmail.com>
2025-05-03 14:35:27 +0100

    initial commit, PoC for bluetooth HID & SSD1306 display

---
+++ kbd_firmware/src/hid.h
@@ -0,0 +1,21 @@
+/** @file
+ *  @brief HoG Service sample
+ */
+
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+ void hid_init(void);
+ 
+ void hid_button_loop(void);
+ 
+ #ifdef __cplusplus
+ }
+ #endif
